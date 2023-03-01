eddf005b6443c7b3446de3ed43a3f453cf4d3b48
Anuvrat Joshi <joshianuvrat@gmail.com>
2023-03-01 17:41:47 +0530

    Added Projects

--- src/component/Project.js
+++ src/component/Project.js
@@ -6,14 +6,14 @@ const Projects = () => {
       <div className="project-list">
       {/* <img src={background} alt='background'/> */}
         <div className="project">
-          <img src="https://via.placeholder.com/150" alt="Project 1" />
-          <h3>Project 1</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        <a href="https://react-movie-search-00hm.onrender.com"><img src="https://via.placeholder.com/150" alt="Project 1" />
+          <h3>Movie-search</h3></a>
+          <p>Movie search app using fetch Api..</p>
         </div>
         <div className="project">
-          <img src="https://via.placeholder.com/150" alt="Project 2" />
-          <h3>Project 2</h3>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+          <a href="https://react-todo-app-qsiz.onrender.com"><img src="https://via.placeholder.com/150" alt="Project 2" />
+          <h3>Todo-App</h3></a>
+          <p>Created todo app using React</p>
         </div>
         <div className="project">
           <img src="https://via.placeholder.com/150" alt="Project 3" />
